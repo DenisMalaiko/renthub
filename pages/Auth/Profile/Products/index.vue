85ee229eea7500c18303661f60c1aacfe3fec72d
denysmalaiko <malaiko.denis@gmail.com>
2024-12-19 18:23:59 +0100

    Profile | Added ability to display list of categories

--- pages/Auth/Profile/Products/index.vue
+++ pages/Auth/Profile/Products/index.vue
@@ -1,15 +1,20 @@
 <template>
   <section class="profile-section">
     Products
+
+    <pre>{{ list }}</pre>
   </section>
 </template>
 
 <script lang="ts" setup>
 import {definePageMeta} from "~/.nuxt/imports";
+import {useProductsLogic} from "~/pages/Auth/Profile/Products/products";
 
 definePageMeta({
   layout: "profile"
-})
+});
+
+const { list } = useProductsLogic();
 </script>
 
 <style>
