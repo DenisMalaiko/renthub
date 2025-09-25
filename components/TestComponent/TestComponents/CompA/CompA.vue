a6d48e1a0f8e104e370f5f79ff15678a87dd20d4
denysmalaiko <malaiko.denis@gmail.com>
2025-09-25 20:13:25 +0200

    Improvements

---
+++ components/TestComponent/TestComponents/CompA/CompA.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const counter = ref(0);
+const count = () => {
+  counter.value++;
+}
+</script>
+
+<template>
+  <div>
+    <h3>Comp A {{ counter }}</h3>
+    <v-btn @click="count">Count</v-btn>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
